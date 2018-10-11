7956bd525fd95a7bfdd046b7a21918dd01e94df5
Energy1190 <karpov@iso.ru>
2018-10-11 19:10:43 +0300

    +

--- includes/docker_include.py
+++ includes/docker_include.py
@@ -6,7 +6,7 @@ from traceback import format_exc
 CONFIG = []         # list names
 PROGRAM = []        # list names
 DEPEND = {}         # depend dict
-SELFDIR = os.path.join('/'.join(os.path.realpath(__file__).split('/')[:-1]), 'docker')        # path
+SELFDIR = os.path.join('/'.join(os.path.realpath(__file__).split('/')[:-1]), 'docker_files')        # path
 
 def get_docker_lib():
     try:
