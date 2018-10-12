578d9e2e00ef6d796a546dcd38cf565ed998bcb9
Energy1190 <karpov@iso.ru>
2018-10-12 16:13:05 +0300

    +

--- includes/docker_files/docker_prog.py
+++ includes/docker_files/docker_prog.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import docker
 from plumbum import cli
+from traceback import format_exc
 
 class MyApp(cli.Application):
     def main(self, *args):
-        print("Hello, it's test.")
-        sys.exit(0)
+        try:
+            name = '.'.join(os.path.realpath(__file__).split('/')[-1].replace('docker_prog_','').split('.')[:-1])
+            client = docker.client.DockerClient(base_url=os.environ.get('DOCKER_SOCKET'))
+            container = client.containers.get(name)
+
+            print(name, container.status)
+            sys.exit(0)
+        except:
+            print(format_exc())
+            sys.exit(1)
 
 if __name__ == "__main__":
     MyApp.run()
