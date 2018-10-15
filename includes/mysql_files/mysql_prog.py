24737842611299fc4510b6930d3b0c8e2382d62b
Energy1190 <karpov@iso.ru>
2018-10-15 18:40:20 +0300

    +

---
+++ includes/mysql_files/mysql_prog.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pymysql
+from plumbum import cli
+from traceback import format_exc
+
+class MyApp(cli.Application):
+    def main(self, *args):
+        max_timewait = 0
+        max_connection = 0
+
+        try:
+            max_tc = '.'.join(os.path.realpath(__file__).split('/')[-1].replace('mysql_prog_','').split('.')[:-1])
+            max_connection, max_timewait = max_tc.split('_')
+        except:
+            print('Error in {}.'.format(os.path.realpath(__file__)))
+            print(format_exc())
+            sys.exit(1)
+
+        hostname = os.environ.get('MYSQL_HOST')
+        username = os.environ.get('MYSQL_USER')
+        password = os.environ.get('MYSQL_PASSWORD')
+        database = os.environ.get('MYSQL_DATABASE')
+
+        if not all([hostname,username,password,database]):
+            print('Failed to get variables.')
+            sys.exit(1)
+
+        db = pymysql.connect(hostname,username,password,database)
+        cursor = db.cursor()
+        cursor.execute("SHOW FULL PROCESSLIST")
+        data = cursor.fetchall()
+
+        if len(data) > int(max_connection):
+            print('Too many database connections: {}'.format(str(len(data))))
+            sys.exit(1)
+
+        timewaits = [(item[5],item[7]) for item in data if int(item[5]) > int(max_timewait)]
+        if len(timewaits):
+            print('The database receives very long querys.')
+            [ print(item) for item in timewaits]
+            sys.exit(1)
+
+        sys.exit(0)
+
+if __name__ == "__main__":
+    MyApp.run()
---
+++ includes/mysql_include.py
@@ -0,0 +1,84 @@
+import os
+import importlib
+from shutil import copyfile
+from traceback import format_exc
+
+CONFIG = []         # list names
+PROGRAM = []        # list names
+DEPEND = {}         # depend dict
+SELFDIR = ''        # path
+
+def get_mysql_lib():
+    try:
+        lib = importlib.import_module('pymysql')
+    except:
+        print('No library found. Try to install.')
+        print(format_exc())
+
+        os.system('pip3 install pymysql')
+        lib = get_mysql_lib()
+    return lib
+
+def get_mysql_client(mysql, envs):
+    try:
+        importlib.reload(mysql)
+        client = mysql.connect(envs.get('MYSQL_HOST'),envs.get('MYSQL_USER'),
+                               envs.get('MYSQL_PASSWORD'),envs.get('MYSQL_DATABASE'))
+    except:
+        print('Unable to connect to docker socket.')
+        print(format_exc())
+
+        return None
+    return client
+
+def build_depend(envs, depend):
+    envs.add_env('MYSQL_CHECK', {'default': 'disable', 'requered': False, 'value': None})
+    envs.add_env('MYSQL_HOST', {'default': 'localhost', 'requered': False, 'value': None})
+    envs.add_env('MYSQL_USER', {'default': 'root', 'requered': False, 'value': None})
+    envs.add_env('MYSQL_PASSWORD', {'default': None, 'requered': False, 'value': None})
+    envs.add_env('MYSQL_DATABASE', {'default': 'mysql', 'requered': False, 'value': None})
+    envs.add_env('MYSQL_MAXCONN', {'default': 200, 'requered': False, 'value': None})
+    envs.add_env('MYSQL_MAXTIME', {'default': 360, 'requered': False, 'value': None})
+    if envs.get('MYSQL_CHECK') == 'disable': return None
+
+    mysql_lib = get_mysql_lib()
+    mysql_client = get_mysql_client(mysql_lib,envs)
+    if not mysql_client:
+        print('Failed to establish a connection to the database.')
+        return None
+    else:
+        mysql_client.close()
+
+    global CONFIG, PROGRAM
+    config = 'mysql_conf'
+    path = os.path.join(SELFDIR, 'mysql_conf' + '.tmpl')
+    path_new = os.path.join(SELFDIR, config + '.tmpl')
+    copyfile(path, path_new)
+    CONFIG.append(config)
+
+    prog = 'mysql_prog_{}_{}'.format(str(envs.get('MYSQL_MAXCONN')), str(envs.get('MYSQL_MAXTIME')))
+    path = os.path.join(SELFDIR, 'mysql_prog' + '.py')
+    path_new = os.path.join(SELFDIR, prog + '.py')
+    copyfile(path, path_new)
+    PROGRAM.append(prog)
+
+    depend.config[config] = {'envs':  ['ATTEMPTS','FAILURES', 'TIMEOUT'],
+                             'parms': {'name': (depend._simple, prog),
+                                       'times': (depend.env.get, 'FAILURES'),
+                                       'cycles': (depend.env.get, 'ATTEMPTS'),
+                                       'timeout': (depend.env.get, 'TIMEOUT')},
+                             'callback': (depend._callback, config)}
+def get(name):
+    if name == 'config': return CONFIG
+    if name == 'program': return PROGRAM
+
+def callback(temlate_path):
+    for item in CONFIG:
+        path = os.path.join(SELFDIR,item + '.tmpl')
+        path_new = os.path.join(temlate_path,item + '.tmpl')
+        if os.path.exists(path): copyfile(path,path_new)
+
+    for item in PROGRAM:
+        path = os.path.join(SELFDIR,item + '.py')
+        path_new = os.path.join(temlate_path,item + '.py')
+        if os.path.exists(path): copyfile(path,path_new)
